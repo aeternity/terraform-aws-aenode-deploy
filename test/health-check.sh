fe43e6a6ab824c59a3067bf249f71af646dcceb4
Valeri Filipov <14542503+valerifilipov@users.noreply.github.com>
2019-05-15 16:44:48 +0300

    fail on empty ips var

--- test/health-check.sh
+++ test/health-check.sh
@@ -3,9 +3,8 @@
 set -Eeuo pipefail
 
 export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-ap-southeast-2}
-export AWS_DEFAULT_OUTPUT=${AWS_DEFAULT_OUTPUT:-text}
 
-IPS=$(aws ec2 describe-instances --filters "Name=tag:envid,Values=${ENVID:?}" --query "Reservations[*].Instances[*].PublicIpAddress" | tr '\t' '\n')
-test -n "$IPS" || echo "No instances found with envid: ${ENVID} in ${AWS_DEFAULT_REGION} region"
+IPS=$(aws ec2 describe-instances --filters "Name=tag:envid,Values=${ENVID:?}" |jq -r '.Reservations[].Instances[].PublicIpAddress')
+test -n "$IPS" || (echo "No instances found with envid: ${ENVID} in ${AWS_DEFAULT_REGION} region" >&2; exit 1)
 
 echo "$IPS" | xargs -n 1 -I '{IP}' curl -s -f -S -o /dev/null --retry 8 --retry-connrefused http://{IP}:8080/healthz
