e1ae7168915045cc3a5b5952ee528cef74415aa8
Ivaylo Badinov <632282+venimus@users.noreply.github.com>
2019-06-20 15:27:30 +0300

    Expose fleet submodule vars (#6)

--- variables.tf
+++ variables.tf
@@ -79,3 +79,12 @@ variable "user_data_file" {
 variable "spot_user_data_file" {
   default = "user_data.bash"
 }
+
+# Exopose fleet module vars
+variable "lb_stickiness_enabled" {
+  default = false
+}
+
+variable "lb_stickiness_cookie_duration" {
+  default = 86400
+}
